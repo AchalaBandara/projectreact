c5f49d8ccbd5aa80e6ef4d24222f592135f7462f
Achala Bandara <hmapb1994@gmail.com>
2020-05-20 16:04:33 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Tweet from "./Tweet";
+import { isRecordExpression } from "@babel/types";
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: "Ed", message: "Hello there", likes: "200k" },
+    { name: "John", message: "I am John snow", likes: "350k" },
+    { name: "Traversy", message: "I am awesome", likes: "600k" }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} likes={user.likes} />
+      ))}
     </div>
   );
 }
